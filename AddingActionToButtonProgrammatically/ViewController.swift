46c2e70922366eedc82785c1ae6e11d0b0df4566
Rajesh Kommana <rajeshkommana@Rajeshs-MacBook-Pro.local>
2017-06-15 18:07:04 +0800

    Added action to button programmatically

--- AddingActionToButtonProgrammatically/ViewController.swift
+++ AddingActionToButtonProgrammatically/ViewController.swift
@@ -9,16 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Declaration button
+    var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // view background color to dark gray
+        self.view.backgroundColor = UIColor.darkGray
+        
+        // Initialization button with type system
+        button = UIButton(type: UIButtonType.system)
+        
+        // Creating frame for button using CGRect CGFloat
+        button.frame = CGRect(x: 0, y: 0, width: 150, height: 60)
+        
+        // Center button horizontally and vertically
+        button.center.x = self.view.center.x
+        button.center.y = self.view.center.y
+        
+        // Creating background color as yellow
+        button.backgroundColor = UIColor.yellow
+        
+        // Creating tint color as orange
+        button.tintColor = UIColor.orange
+        
+        // Set title for button
+        button.setTitle("Button Action", for: UIControlState.normal)
+        
+        // Adding action to button using addTarget method
+        button.addTarget(self, action: #selector(ViewController.actionButton(_:)), for: UIControlEvents.touchUpInside)
+        
+        // Adding button to view
+        self.view.addSubview(button)
+        
+        
+        
+    }
+    
+    func actionButton(_ actionButton: UIButton!) {
+        
+        // Changing title color
+        button.setTitleColor(.blue, for: UIControlState.normal)
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
